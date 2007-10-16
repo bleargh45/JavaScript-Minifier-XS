562bb2f170860bbdf4eaa793a62415b03688d27c
Graham TerMarsch <graham@howlingfrog.com>
2007-10-16 19:26:13 +0000

    Relocate the XS file, os that {{{ExtUtils::MakeMaker}}} picks it up properly when doing a build.

--- lib/JavaScript/Minifier/XS.pm
+++ lib/JavaScript/Minifier/XS.pm
@@ -8,7 +8,7 @@ require DynaLoader;
 our @ISA = qw(Exporter DynaLoader);
 our @EXPORT_OK = qw(minify);
 
-our $VERSION = '0.01_01';
+our $VERSION = '0.01_02';
 
 bootstrap JavaScript::Minifier::XS $VERSION;
 
