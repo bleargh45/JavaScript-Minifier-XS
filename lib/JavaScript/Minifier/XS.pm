5006f8b15f17473f43685ee7ff34df72097169b0
Graham TerMarsch <graham@howlingfrog.com>
2011-06-30 09:50:29 -0700

    Version bump.

--- lib/JavaScript/Minifier/XS.pm
+++ lib/JavaScript/Minifier/XS.pm
@@ -9,7 +9,7 @@ require DynaLoader;
 our @ISA = qw(Exporter DynaLoader);
 our @EXPORT_OK = qw(minify);
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 bootstrap JavaScript::Minifier::XS $VERSION;
 
