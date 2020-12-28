6a2f630e433a71f734bcbfae738064b13c2b8d3f
Graham TerMarsch <graham@howlingfrog.com>
2020-12-28 08:32:04 -0800

    Release v0.12

--- lib/JavaScript/Minifier/XS.pm
+++ lib/JavaScript/Minifier/XS.pm
@@ -9,7 +9,7 @@ require DynaLoader;
 our @ISA = qw(Exporter DynaLoader);
 our @EXPORT_OK = qw(minify);
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 bootstrap JavaScript::Minifier::XS $VERSION;
 
